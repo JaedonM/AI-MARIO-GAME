45ea4a35648bbea37f497106947f16168d29cbab
JaedonM <114765003+JaedonM@users.noreply.github.com>
2023-06-30 09:51:28 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+noseX=0
+noseY=0
+marioX=325
+marioY=325
+
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent("canvas")
+	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800,400);
+    video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded)
+	poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded(){
+console.log("Model has been activated")
+}
+
+function gotPoses(results){
+if (results.length>0) {
+	console.log(results)
+	noseX = results[0].pose.nose.x
+	noseY = results[0].pose.nose.y
+}
+}
+
+function draw(){
+	game()
+
+	if (noseX < 400) {
+		marioX = marioX - 1
+	}
+
+	if (noseX > 400) {
+		marioX = marioX + 1
+	}
+}
+
+
+
+
+
+
